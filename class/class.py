4fecbaaed291eca0f1ba55da3dac1cf8ea740103
12avni.sharma <avni97@gmail.com>
2023-04-22 00:29:07 -0700

    new project

--- class/class.py
+++ class/class.py
@@ -1,6 +1,11 @@
 class User:
+    def __init__(self, id, username) -> None:
+        self.id = id
+        self.username = username
+    def follow (self, followers, following):
+        
+        
     pass
-user_1 = User()
-user_1.id = "001"
+user_1 = User("001", "Avni")
 
 print(user_1.id)
