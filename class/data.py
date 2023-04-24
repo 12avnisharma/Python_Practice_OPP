83c323f328d0bb9a0cc590d3f09aacaebbef0a23
12avni.sharma <avni97@gmail.com>
2023-04-24 15:46:32 -0700

    Final Quiz using trivia database

---
+++ class/data.py
@@ -0,0 +1,17 @@
+question_data = [
+    {
+    "category":"Science & Nature",
+    "type":"boolean",
+    "difficulty":"medium",
+    "question":"The Neanderthals were a direct ancestor of modern humans.","correct_answer":"False",
+    "incorrect_answers":["True"]},
+    {"category":"Science & Nature",
+     "type":"boolean",
+     "difficulty":"medium",
+     "question":"A person can get sunburned on a cloudy day.",
+     "correct_answer":"True",
+     "incorrect_answers":["False"]},
+    {"category":"Science & Nature","type":"boolean","difficulty":"medium","question":"Hippopotomonstrosesquippedaliophobia is the irrational fear of long words.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science & Nature","type":"boolean","difficulty":"medium","question":"The planet Mars has two moons orbiting it.","correct_answer":"True","incorrect_answers":["False"]},
+    {"category":"Science & Nature","type":"boolean","difficulty":"medium","question":"Like with the Neanderthals, Homo sapiens sapiens also interbred with the Denisovans.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science & Nature","type":"boolean","difficulty":"medium","question":"Shrimp can swim backwards.","correct_answer":"True","incorrect_answers":["False"]},
+    {"category":"Science & Nature","type":"boolean","difficulty":"medium","question":"Anatomy considers the forms of macroscopic structures such as organs and organ systems.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science & Nature","type":"boolean","difficulty":"medium","question":"In the periodic table, Potassium&#039;s symbol is the letter K.","correct_answer":"True","incorrect_answers":["False"]},
+    {"category":"Science & Nature","type":"boolean","difficulty":"medium","question":"Pneumonoultramicroscopicsilicovolcanoconiosis is a synonym for the disease known as silicosis.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Science & Nature","type":"boolean","difficulty":"medium","question":"&quot;Tachycardia&quot; or &quot;Tachyarrhythmia&quot; refers to a resting heart-rate near or over 100 BPM.","correct_answer":"True","incorrect_answers":["False"]}]
